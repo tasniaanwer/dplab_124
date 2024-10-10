005d04266844f18193e625f936fa596c297d8a63
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2024-10-10 21:53:19 +0600

    Update Sugar.java

--- lab4/Sugar.java
+++ lab4/Sugar.java
@@ -1,15 +1,7 @@
-class Sugar implements CondimentDecorator {
-    private Beverage beverage;
-
-    public Sugar(Beverage beverage) {
-        this.beverage = beverage;
+public class Sugar extends Condiment
+{
+    public Sugar() {
+        super("Sugar", 15);
     }
 
-    public String getDescription() {
-        return beverage.getDescription() + ", Sugar";
-    }
-
-    public double cost() {
-        return 20 + beverage.cost();
-    }
 }
