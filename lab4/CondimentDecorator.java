13a0b57002fd0798a5b82736d21f560da996930f
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2024-10-10 21:49:08 +0600

    Update CondimentDecorator.java

--- lab4/CondimentDecorator.java
+++ lab4/CondimentDecorator.java
@@ -1,3 +1,17 @@
-interface CondimentDecorator extends Beverage {
+public abstract class Condiment {
+    private String name;
+    private double cost;
 
+    public Condiment(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
 }
